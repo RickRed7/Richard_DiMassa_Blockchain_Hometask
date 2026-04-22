d4fd31fba2bc8fad3310469ac444588fc89f9436
Richard A DiMassa <rickitt23@gmail.com>
2026-04-22 14:02:47 -0700

    Add wallet generation functionality

--- controllers/wallet.controller.js
+++ controllers/wallet.controller.js
@@ -1,1 +1,16 @@
+const crypto = require('crypto');
+const { sendSuccess, sendError } = require('../utils/response');
 
+exports.generateWallet = (req, res) => {
+  try {
+      const { publicKey, privateKey } = crypto.generateKeyPairSync('ec', {
+            namedCurve: 'secp256k1',
+                  publicKeyEncoding: { type: 'spki', format: 'pem' },
+                        privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
+                            });
+                                return sendSuccess(res, 'Wallet generated successfully', { publicKey, privateKey });
+                                  } catch (error) {
+                                      return sendError(res, 'Failed to generate wallet', 500);
+                                        }
+                                        };
+                                        
