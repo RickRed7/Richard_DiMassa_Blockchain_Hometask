a7a1b9835dbd686888ed638722c0f488e36c3a40
Richard A DiMassa <rickitt23@gmail.com>
2026-04-22 14:04:35 -0700

    Add wallet generation route

--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -1,1 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const walletController = require('../controllers/wallet.controller');
 
+router.post('/', walletController.generateWallet);
+
+module.exports = router;
